657b9040f849f3bb8a0cfa055a2084b93fcba580
Js-Yang <chyang590@gmail.com>
2018-12-01 17:43:18 +0800

    pass

--- Kata91_betdto/UnitTest1.cs
+++ Kata91_betdto/UnitTest1.cs
@@ -6,22 +6,44 @@ namespace Kata91_betdto
     [TestClass]
     public class UnitTest1
     {
+        protected internal BetConverter BetConverter = new BetConverter();
+
         [TestMethod]
         public void Convert_To_BetDto()
         {
-            var betConverter = new BetConverter();
-            var bet = new Bet() { Id = 123, CreatedTime = new DateTime(2018, 12, 1), Stake = 100 };
-
-            var actual = betConverter.Convert(bet, ConvertRule());
+            var actual = BetConverter.Convert(GenerateBet(), ConvertRule());
 
             Assert.AreEqual(100, actual.Amount);
             Assert.AreEqual(123, actual.BetId);
             Assert.AreEqual("20181201", actual.Date);
         }
 
+        [TestMethod]
+        public void Convert_To_BetDto_With_Interface()
+        {
+            var actual = BetConverter.ConvertFromMap<Bet, BetDto>(GenerateBet(), new BetMapper());
+
+            Assert.AreEqual(100, actual.Amount);
+            Assert.AreEqual(123, actual.BetId);
+            Assert.AreEqual("20181201", actual.Date);
+        }
+
+        private static Bet GenerateBet()
+        {
+            return new Bet() { Id = 123, CreatedTime = new DateTime(2018, 12, 1), Stake = 100 };
+        }
+
         private static Func<Bet, BetDto> ConvertRule()
         {
             return (b) => new BetDto() { Amount = (int)b.Stake, BetId = b.Id, Date = b.CreatedTime.ToString("yyyyMMdd") };
+        }
+    }
+
+    public class BetMapper : IBetMapper<Bet, BetDto>
+    {
+        public BetDto Mapping(Bet bet)
+        {
+            return new BetDto() { Amount = (int)bet.Stake, BetId = bet.Id, Date = bet.CreatedTime.ToString("yyyyMMdd") };
         }
     }
 
@@ -38,6 +60,16 @@ namespace Kata91_betdto
         {
             return convertRule(bet);
         }
+
+        public TResult ConvertFromMap<TSource, TResult>(TSource bet, IBetMapper<TSource, TResult> mapper)
+        {
+            return mapper.Mapping(bet);
+        }
+    }
+
+    public interface IBetMapper<TSource, TResult>
+    {
+        TResult Mapping(TSource bet);
     }
 
     public class BetDto
